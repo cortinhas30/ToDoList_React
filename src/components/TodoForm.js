071d3a3c43f28afe3f4a62d142a54fd4badfa3eb
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2022-11-17 20:46:59 -0300

    gravando to-do

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,10 +10,10 @@ function TodoForm(props) {
   const handleSubmit = e => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
 
     setInput('');
   };
@@ -28,7 +28,7 @@ function TodoForm(props) {
         className="todo-input"
         onChange={handleChange}
       />
-      <button classname='todo-button'>Adicionar tarefa</button>
+      <button className='todo-button'>Adicionar tarefa</button>
     </form>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,23 @@ import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+    const addTodo = todo => {
+      if(!todo.text || /^\s*$/.test(todo.text)){
+        return;
+      }
+
+      const newTodos = [todo, ...todos];
+
+      setTodos(newTodos);
+      console.log(...todos);
+
+    }
+
   return (
     <div>
         <h1>qual seu plano pra hoje meu nobre?</h1>
-        <TodoForm />
+        <TodoForm onSubmit={addTodo} />
     </div>
   )
 }
