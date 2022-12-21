06536e6cc85ba71906e950ad4fa10f0b197999e9
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2022-12-21 19:10:03 -0300

    todo icons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
+
     const addTodo = todo => {
       if(!todo.text || /^\s*$/.test(todo.text)){
         return;
@@ -11,16 +13,25 @@ function TodoList() {
       const newTodos = [todo, ...todos];
 
       setTodos(newTodos);
-      console.log(...todos);
+    };
 
+    const completeTodo = id => {
+      let updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
     }
 
   return (
     <div>
         <h1>qual seu plano pra hoje meu nobre?</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} />
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
